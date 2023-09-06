586f58d12516d0eb6c3668bba7cb2d0c9d84f18b
Zach1281 <zachary_romero@yahoo.com>
2023-09-05 20:34:39 -0700

    Add styling for page

--- src/website.js
+++ src/website.js
@@ -1,13 +1,7 @@
 import './style.css';
 
-export default function component() {
+export default function siteInit() {
     const main = document.querySelector('.content');
-
-    const title = document.createElement('h1');
-    title.classList.add('title');
-    title.textContent = "Todo App"
-
-    main.appendChild(title);
-
+    
     return main;
 }
